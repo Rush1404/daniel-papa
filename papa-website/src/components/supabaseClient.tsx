a1b21b99b3307114b22090029ca17f18e377436e
Rush1404 <96493071+Rush1404@users.noreply.github.com>
2026-02-20 22:17:47 +0000

    env var bug fix

--- papa-website/src/components/supabaseClient.tsx
+++ papa-website/src/components/supabaseClient.tsx
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Create it OUTSIDE of any function or component
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
